5374a7ea5577291ca9c89d5f0ae10d4ff05d55bb
Adrien Milis <adrien.milis@brussels.msf.org>
2026-07-28 10:16:47 +0200

    Remove standard SQL constraints that raise warnings

--- addons/hr_holidays/models/hr_leave_allocation.py
+++ addons/hr_holidays/models/hr_leave_allocation.py
@@ -125,7 +125,7 @@ class HolidaysAllocation(models.Model):
     expiring_carryover_days = fields.Float("The number of carried over days that will expire on carried_over_days_expiration_date")
     carried_over_days_expiration_date = fields.Date("Carried over days expiration date")
     _sql_constraints = [
-        ('duration_check', "CHECK( ( number_of_days > 0 AND allocation_type='regular') or (allocation_type != 'regular'))", "The duration must be greater than 0."),
+        # ('duration_check', "CHECK( ( number_of_days > 0 AND allocation_type='regular') or (allocation_type != 'regular'))", "The duration must be greater than 0."),
     ]
 
     @api.constrains('date_from', 'date_to')
